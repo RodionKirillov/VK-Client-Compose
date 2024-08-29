e360512073653b8856c10db95c2235e4b59966a9
RodionKirillov <rodion.44454@gmail.com>
2024-08-29 10:24:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vkclientcompose/ui/theme/PostCard.kt
@@ -0,0 +1,22 @@
+package com.example.vkclientcompose.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun PostCard() {
+    Card {
+        Row {
+            Image(painter = , contentDescription = )
+            Column {
+
+            }
+            Image(painter =, contentDescription =)
+        }
+    }
+}
