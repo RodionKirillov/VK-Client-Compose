9676417cce8a04a810836219d1384a5e7987d4cd
RodionKirillov <rodion.44454@gmail.com>
2024-09-01 12:52:47 +0300

    3.3-State

--- app/src/main/java/com/example/vkclientcompose/MainActivity.kt
+++ app/src/main/java/com/example/vkclientcompose/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.vkclientcompose.ui.theme.ScaffoldTest
+import com.example.vkclientcompose.ui.theme.MainScreen
 import com.example.vkclientcompose.ui.theme.VKClientComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             VKClientComposeTheme {
-                ScaffoldTest()
+                MainScreen()
 //                Box(
 //                    modifier = Modifier
 //                        .fillMaxSize()
